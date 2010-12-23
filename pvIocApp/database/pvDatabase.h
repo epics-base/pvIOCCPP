abc501233394cb80c40ac77b78e0f65f8313d008
Marty Kraimer <mrkraimer@comcast.net>
2010-12-23 09:57:57 -0500

    creating project structure

---
+++ pvIocApp/database/pvDatabase.h
@@ -0,0 +1,16 @@
+/* pvDataBase.h */
+/**
+ * Copyright - See the COPYRIGHT that is included with this distribution.
+ * EPICS pvDataCPP is distributed subject to a Software License Agreement found
+ * in file LICENSE that is included with this distribution.
+ */
+#include <string>
+#include <stdexcept>
+#ifndef PVDATABASE_H
+#define PVDATABASE_H
+
+namespace epics { namespace pvData { 
+
+    
+}}
+#endif  /* PVDATABASE_H */
